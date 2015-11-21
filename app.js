414e5576276ae960126d18df391420d2590f7c8e
Joseph Harding <joehharding@gmail.com>
2015-11-21 15:25:06 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var app = express();
+
+app.listen(3700);
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/public/index.html"); 
+});
+
+app.use(express.static(__dirname + '/public'));
